0bd108a20a4828c0f1028e84b1bf9286a67390af
Vladimir Chichetkin <chivladimir@yandex.ru>
2025-01-31 09:16:39 +0300

    Training

---
+++ .venv/Python_task_book_1_1.py
@@ -0,0 +1,28 @@
+x = int(input())
+if x < 0:
+    print("Negative number")
+else:
+    print("Positive number")
+
+if x%2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
+
+
+
+x_X = str(input())
+y_Y = str(input())
+print (f"Length of the string is {len(x_X)}")
+print (f"Last symbol of the string is {x_X[-1]}")
+if x_X == y_Y:
+    print("The first symbols is the same")
+else:
+    print("The first symbols is not the same")
+
+if y_Y[-1] == "z":
+    print (f"Last symbol of the string before {y_Y[-1]} is {y_Y[-2]}")
+else:
+    print(f"Last symbol of the string is not {y_Y[-1]}")
+
+
